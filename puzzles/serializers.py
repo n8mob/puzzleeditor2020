e1fcb9b28897495548d2e2f8662c9deedcfd0937
Nate Grigg <n8mob@nategrigg.com>
2020-06-14 15:29:58 -0600

    add win-message serializer

--- puzzles/serializers.py
+++ puzzles/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 
-from puzzles.models import Line, Puzzle
+from puzzles.models import Puzzle, ClueLine, WinMessageLine
 
 
 class ClueLineSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Line
+        model = ClueLine
         fields = ['text', 'length', 'clue_in']
+
+
+class WinMessageLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WinMessageLine
+        fields = ['text', 'length', 'win_message_in']
 
 
 class PuzzleSerializer(serializers.ModelSerializer):
     clue_lines = serializers.StringRelatedField(many=True)
+    win_message_lines = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Puzzle
-        fields = ['id', 'name', 'clue_lines']
+        fields = ['id', 'name', 'clue_lines', 'win_message_lines']
