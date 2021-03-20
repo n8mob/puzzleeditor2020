52efaacd381b1a895b87998a77c994f14c2e8f46
Nate Grigg <n8mob@nategrigg.com>
2021-03-20 17:00:34 -0600

    add dev settings

--- magie_online/settings_dev.py
+++ magie_online/settings_dev.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'puzzleeditor2020-dev.us-west-2.elasticbeanstalk.com',
+    'localhost',
 ]
 
 # Application definition
