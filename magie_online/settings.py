72ae7e4a8c61d4fc432366a492eb4b04725857bb
Nate Grigg <n8mob@nategrigg.com>
2021-05-15 21:45:31 -0600

    some elastic beanstalk configuration (db and environment name)

--- magie_online/settings.py
+++ magie_online/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    'puzzleeditor2020-env.eba-47ymdsph.us-west-2.elasticbeanstalk.com',
     'puzzleeditor2020-dev.us-west-2.elasticbeanstalk.com',
     'magie-editor.us-west-2.elasticbeanstalk.com',
 ]
@@ -79,13 +80,25 @@ WSGI_APPLICATION = 'magie_online.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('RDS_DB_NAME'),
+            'USER': os.environ.get('RDS_USERNAME'),
+            'PASSWORD': os.environ.get('RDS_PASSWORD'),
+            'HOST': os.environ.get('RDS_HOSTNAME'),
+            'PORT': os.environ.get('RDS_PORT'),
+        },
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
