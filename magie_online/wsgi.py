a831c57c0344a7999e990e5c6f41c44f3ef34be0
Nate Grigg <n8mob@nategrigg.com>
2022-08-17 09:37:29 -0600

    put config selection back into wsgi

--- magie_online/wsgi.py
+++ magie_online/wsgi.py
@@ -11,4 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+environment = os.environ.get('APP_ENVIRONMENT', default='local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'magie_online.settings.{environment}')
+
 application = get_wsgi_application()
