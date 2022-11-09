1e4f658ac9a96887e4e5d480719f3fe7267af942
Nate Grigg <n8mob@nategrigg.com>
2022-11-08 19:03:42 -0700

    fix db path again...

--- magie_online/settings/local.py
+++ magie_online/settings/local.py
@@ -69,7 +69,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
