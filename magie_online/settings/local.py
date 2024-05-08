ef9ef446686acf1cf6afe14c157ec08e54652ee6
Nate Grigg <n8mob@nategrigg.com>
2024-05-08 12:25:25 -0600

    install django analytics instead of google analytics script directly

--- magie_online/settings/local.py
+++ magie_online/settings/local.py
@@ -1,4 +1,3 @@
-import os
 # begin base
 import logging
 import os
@@ -12,85 +11,90 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'puzzleeditor2020.local'
+  'localhost',
+  '127.0.0.1',
+  'puzzleeditor2020.local'
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+  'https://www.google-analytics.com'
+]
+
+GOOGLE_ANALYTICS_GTAG_PROPERTY_ID = 'G-ZL5RKDBBF6'
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'puzzles.apps.PuzzlesConfig',
-    'rest_framework',
-    'char_counter',
-    'django.contrib.staticfiles',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'puzzles.apps.PuzzlesConfig',
+  'rest_framework',
+  'char_counter',
+  'django.contrib.staticfiles',
+  'analytical'
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'magie_online.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/')]
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates/')]
+    ,
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'magie_online.wsgi.application'
 
-
 # Database
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
-    'main': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': os.path.join(BASE_DIR, 'db.bigtest.sqlite3')
-    }
+  'main': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  },
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.bigtest.sqlite3')
+  }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -106,26 +110,25 @@ USE_L10N = True
 USE_TZ = True
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler'
-        },
-        # 'file': {
-        #     'level': 'INFO',
-        #     'class': 'logging.FileHandler',
-        #     'filename': '/opt/python/log/django.log',
-        # },
+  'version': 1,
+  'disable_existing_loggers': False,
+  'handlers': {
+    'console': {
+      'class': 'logging.StreamHandler'
     },
-    'loggers': {
-        'root': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
+    # 'file': {
+    #     'level': 'INFO',
+    #     'class': 'logging.FileHandler',
+    #     'filename': '/opt/python/log/django.log',
+    # },
+  },
+  'loggers': {
+    'root': {
+      'handlers': ['console'],
+      'level': 'INFO',
+      'propagate': True,
     },
+  },
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
-
--- puzzles/urls.py
+++ puzzles/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.PuzzlesListView.as_view()),
-    path('<pk>', views.PuzzleDetailView.as_view()),
-]
+  path('', views.PuzzlesListView.as_view()),
+  path('<pk>', views.PuzzleDetailView.as_view())
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# `static` already configures DEBUG, so no need for this stuff.
+# if settings.DEBUG:
+#   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
