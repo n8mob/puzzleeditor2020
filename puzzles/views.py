d4619f9ac87dd03a215f82b5c8045656b22e137b
Nate Grigg <n8mob@nategrigg.com>
2024-08-02 16:41:23 -0600

    create endpoint for "puzzles/today/"
    to test the endpoint, I'm returning a random puzzle.
    
    That may help in debugging the UI

--- puzzles/views.py
+++ puzzles/views.py
@@ -1,35 +1,54 @@
-from rest_framework import generics
+import datetime
+import logging
+import zoneinfo
 
-from puzzles.models import Puzzle, Level, Menu
-from puzzles.serializers import PuzzleSerializer, LevelSerializer, MenuSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+
+from puzzles.models import Level, Menu, Puzzle
+from puzzles.serializers import LevelSerializer, MenuSerializer, PuzzleSerializer
 
 
 class MenuListView(generics.ListCreateAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
 
 
 class MenuDetailView(generics.RetrieveAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-    lookup_field = 'name'
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+  lookup_field = 'name'
 
 
 class LevelsListView(generics.ListCreateAPIView):
-    queryset = Level.objects.all()
-    serializer_class = LevelSerializer
+  queryset = Level.objects.all()
+  serializer_class = LevelSerializer
 
 
 class LevelDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Level.objects.all()
-    serializer_class = LevelSerializer
+  queryset = Level.objects.all()
+  serializer_class = LevelSerializer
 
 
 class PuzzlesListView(generics.ListCreateAPIView):
-    queryset = Puzzle.objects.all()
-    serializer_class = PuzzleSerializer
+  queryset = Puzzle.objects.all()
+  serializer_class = PuzzleSerializer
 
 
 class PuzzleDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Puzzle.objects.all()
-    serializer_class = PuzzleSerializer
+  queryset = Puzzle.objects.all()
+  serializer_class = PuzzleSerializer
+
+
+class DailyPuzzleView(generics.RetrieveAPIView):
+  queryset = Puzzle.objects.all()
+  serializer_class = PuzzleSerializer
+  lookup_field = 'puzzle_number'
+
+  def get(self, request, *args, **kwargs):
+    local_zone = zoneinfo.ZoneInfo('America/Denver')
+    local_now = datetime.datetime.now(local_zone)
+    random_puzzle = Puzzle.objects.order_by('?').first()
+
+    serializer = self.get_serializer(random_puzzle)
+    return Response(serializer.data)
