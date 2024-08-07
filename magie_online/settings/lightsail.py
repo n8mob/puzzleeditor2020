98e4f99f5d5de6778023a9c7d2174b98744637e7
Nate Grigg <n8mob@nategrigg.com>
2024-08-07 17:54:51 -0600

    test networking

--- magie_online/settings/lightsail.py
+++ magie_online/settings/lightsail.py
@@ -1,6 +1,7 @@
-import os
-import boto3
 import json
+import os
+
+import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -8,30 +9,30 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 
 def get_secret(secret_name):
-    region_name = "us-west-2"
+  region_name = "us-west-2"
 
-    # Create a Secrets Manager client using the assumed role's temporary credentials
-    client = boto3.client(
-        service_name='secretsmanager',
-        region_name=region_name
+  # Create a Secrets Manager client using the assumed role's temporary credentials
+  client = boto3.client(
+    service_name='secretsmanager',
+    region_name=region_name
+  )
+
+  try:
+    get_secret_value_response = client.get_secret_value(
+      SecretId=secret_name
     )
+  except (NoCredentialsError, PartialCredentialsError) as e:
+    raise Exception('AWS credentials not found') from e
+  except client.exceptions.ResourceNotFoundException:
+    raise Exception(f'The requested secret {secret_name} was not found')
+  except client.exceptions.InvalidRequestException as e:
+    raise Exception(f'The request was invalid due to: {e}')
+  except client.exceptions.InvalidParameterException as e:
+    raise Exception(f'The request had invalid params: {e}')
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except (NoCredentialsError, PartialCredentialsError) as e:
-        raise Exception('AWS credentials not found') from e
-    except client.exceptions.ResourceNotFoundException:
-        raise Exception(f'The requested secret {secret_name} was not found')
-    except client.exceptions.InvalidRequestException as e:
-        raise Exception(f'The request was invalid due to: {e}')
-    except client.exceptions.InvalidParameterException as e:
-        raise Exception(f'The request had invalid params: {e}')
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-    return secret
+  # Decrypts secret using the associated KMS key.
+  secret = get_secret_value_response['SecretString']
+  return secret
 
 
 # Retrieve secrets
@@ -39,23 +40,24 @@ SECRET_KEY = get_secret('prod/magiepuzzles/django_secret_key')
 
 database_credentials = json.loads(get_secret('prod/magiepuzzles/postgresql'))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': database_credentials['dbname'],
-        'USER': database_credentials['username'],
-        'PASSWORD': database_credentials['password'],
-        'HOST': database_credentials['host'],
-        'PORT': database_credentials['port'],
-    },
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': database_credentials['dbname'],
+    'USER': database_credentials['username'],
+    'PASSWORD': database_credentials['password'],
+    'HOST': database_credentials['host'],
+    'PORT': database_credentials['port'],
+  },
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'puzzles.magiegame.com',
-    'puzzleeditor2020-dev.us-west-2.elasticbeanstalk.com',
-    'magie-editor.us-west-2.elasticbeanstalk.com',
+  'puzzles.magiegame.com',
+  'localhost:8000',
+  'puzzleeditor2020-dev.us-west-2.elasticbeanstalk.com',
+  'magie-editor.us-west-2.elasticbeanstalk.com',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -65,43 +67,43 @@ CSRF_COOKIE_SECURE = True
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'puzzles.apps.PuzzlesConfig',
-    'rest_framework',
-    'char_counter',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'puzzles.apps.PuzzlesConfig',
+  'rest_framework',
+  'char_counter',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'magie_online.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, '../../templates')]
+    ,
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'magie_online.wsgi.application'
@@ -113,18 +115,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 # Internationalization
@@ -141,25 +143,25 @@ USE_L10N = True
 USE_TZ = True
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler'
-        },
-        # 'file': {
-        #     'level': 'INFO',
-        #     'class': 'logging.FileHandler',
-        #     'filename': '/opt/python/log/django.log',
-        # },
+  'version': 1,
+  'disable_existing_loggers': False,
+  'handlers': {
+    'console': {
+      'class': 'logging.StreamHandler'
     },
-    'loggers': {
-        'root': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
+    # 'file': {
+    #     'level': 'INFO',
+    #     'class': 'logging.FileHandler',
+    #     'filename': '/opt/python/log/django.log',
+    # },
+  },
+  'loggers': {
+    'root': {
+      'handlers': ['console'],
+      'level': 'INFO',
+      'propagate': True,
     },
+  },
 }
 
 STATIC_URL = '/static/'
