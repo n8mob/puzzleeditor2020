4cc1226e82321c4daebb809b9c98f691d1c01227
Nate Grigg <n8mob@nategrigg.com>
2024-08-13 10:25:32 -0600

    remove old cloudfront distribution
    that distribution was replaced by a new one, and we're not using that one

--- magie_online/settings/lightsail.py
+++ magie_online/settings/lightsail.py
@@ -71,7 +71,6 @@ CORS_ALLOWED_ORIGINS = [
   'https://magiegame.com',
   'http://magiegame.local:5173',
   'https://puzzles.magiegame.com',
-  'https://d1ocrz2zrz8geq.cloudfront.net/',
   'https://100.20.81.239',
   'http://100.20.81.239'
 ]
