2a62e95f43bd0d01a046b1b24c770945032677cd
Nate Grigg <n8mob@nategrigg.com>
2026-07-28 12:41:15 -0600

    Save slug-generation code just in case.

--- puzzles/management/commands/generate_puzzle_slugs.py
+++ puzzles/management/commands/generate_puzzle_slugs.py
@@ -26,4 +26,20 @@ class Command(BaseCommand):
       puzzle.save()
 
   def old_auto_slug_for_safe_keeping(self, *args, **options):
-    pass
+    for puzzle in Puzzle.objects.all():
+      if not puzzle.slug and puzzle.name:
+        raw_slug = slugify(puzzle.name)
+        if not raw_slug:
+          raw_slug = f'{puzzle.level.slug}-puzzle-{puzzle.puzzle_number}'
+
+        self.stdout.write(
+          f"Puzzle {puzzle.id}: {puzzle.name} → {puzzle.slug}"
+        )
+
+        puzzle.slug = raw_slug[:250]
+        # could error if not unique
+        # would it error on `self.slug = ...` assignment, or here on `.save()`?
+        # doesn't really matter if slug is required (which I think it is)
+        # if slug is required and not present, we'll get an error.
+        # here we're just trying to back-fill the slug from the name.
+        puzzle.save()
